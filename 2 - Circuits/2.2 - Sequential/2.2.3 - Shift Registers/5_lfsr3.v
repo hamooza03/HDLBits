679d7093c4c6a5b0fe0679e7be944f70895ad18e
hamooza03 <abualkhair.hamza03@gmail.com>
2025-12-27 14:27:38 -0500

    Vectors and shifting!

--- 2 - Circuits/2.2 - Sequential/2.2.3 - Shift Registers/5_lfsr3.v
+++ 2 - Circuits/2.2 - Sequential/2.2.3 - Shift Registers/5_lfsr3.v
@@ -3,21 +3,10 @@ module top_module (
 	input [1:0] KEY,     // L and clk
 	output [2:0] LEDR);  // Q
     
-    // Rename KEY to retreive clk and L signals
-    wire clk, L;
-    assign clk = KEY[0];
-    assign L = KEY[1];
-    
-    // Rename SW to r..
-    wire r0, r1, r2;
-    assign r0 = SW[0];
-    assign r1 = SW[1];
-    assign r2 = SW[2];
-    
-    // Inputs to registers
-    wire d0, d1, d2;
-    
-    
-
+    always @(posedge KEY[0]) begin
+        LEDR[0] <= KEY[1] ? SW[0] : LEDR[2];
+        LEDR[1] <= KEY[1] ? SW[1] : LEDR[0];
+        LEDR[2] <= KEY[1] ? SW[2] : {LEDR[2] ^ LEDR[1]};
+    end
 
 endmodule
